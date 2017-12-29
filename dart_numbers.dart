92bef583585718146c55e159cea7152b85fbd68c
Anwesh43 <anweshthecool0@gmail.com>
2017-12-29 23:13:47 +0530

    adding dart numbers demo

---
+++ dart_numbers.dart
@@ -0,0 +1,11 @@
+void main() {
+    int a = 20;
+    bool odd = a.isOdd;
+    bool even = a.isEven;
+    int sign = a.sign;
+    bool negative = a.isNegative;
+    print("Is $a odd $odd");
+    print("Is $a odd $even");
+    print("sign of $a is $sign");
+    print("$a is negative $negative");
+}
